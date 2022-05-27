2dd585a00bfe71a35bba1f018528a9c65fb26771
James Baum <james@jamesbaum.co.uk>
2022-05-27 09:03:25 +0100

    Set up vscode and add state

---
+++ new/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    'import/named': 0,
+    'prettier/prettier': 'error',
+    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'import/no-named-as-default': 0,
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+  ],
+  env: {
+    es6: true,
+    jest: true,
+  },
+  globals: {},
+  settings: {},
+};
--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useRef, useState } from 'react';
-// import './App.scss';
+import classNames from 'classnames';
+import React, { FormEvent, useRef, useState } from 'react';
 import './styles/core.scss';
-import UploadForm from './UploadForm';
+// import UploadForm from './UploadForm';
 import NoWorkerConverter, { Options, Pixels } from './utils/noWorkerConverer';
 import BackendForm from './components/BackendForm';
-import classNames from 'classnames';
 
 const noWorkerConverter = new NoWorkerConverter();
 
@@ -15,42 +14,40 @@ const noWorkerConverter = new NoWorkerConverter();
 //   whitespace: boolean,
 //   numWorkers: number
 // }
-function handleImageUpload(imageData: Pixels, options: Options) {
-  noWorkerConverter.toAscii(
-    imageData,
-    options
-  );
+
+function showImagePreview(
+  canvasContext: CanvasRenderingContext2D,
+  image: CanvasImageSource,
+) {
+  console.log('drawiign');
+  canvasContext.drawImage(image, 0, 0);
 }
 
-const allowedTypes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png'
-    ];
+function handleImageUpload(imageData: Pixels, options: Options) {
+  noWorkerConverter.toAscii(imageData, options);
+}
+
+const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
 
 function App() {
-  const [file, setFile] = useState<HTMLImageElement | null>(null);
+  const [file, setFile] = useState('');
   const [dragEnter, setDragEnter] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const input = useRef(null);
-  const photo = useRef(null);
+  const input = useRef(null); // file input
+  const photo = useRef(null); // canvas
 
   const handleDataReceived = (data: any) => {
     console.log('got some data', data);
-  }
+  };
 
   const handleImageComplete = (data: any) => {
     console.log('complete', data);
-  }
+  };
 
-  noWorkerConverter.on("progress", (data: any) =>
-    handleDataReceived(data)
-  );
-  noWorkerConverter.on("result", handleImageComplete);
-
-
+  noWorkerConverter.on('progress', (data: any) => handleDataReceived(data));
+  noWorkerConverter.on('result', handleImageComplete);
 
   const onClick = () => {
     // use file upload
@@ -62,21 +59,21 @@ function App() {
     } else {
       console.error('No input ref set');
     }
-  }
+  };
 
   const onDragEnter = (e: any) => {
     e.preventDefault();
     setDragEnter(true);
-  }
+  };
 
   const onDragOver = (e: any) => {
     e.preventDefault();
-  }
+  };
 
   const onDragLeave = (e: any) => {
     e.preventDefault();
     setDragEnter(false);
-  }
+  };
 
   const onDrop = (e: any) => {
     e.preventDefault();
@@ -93,62 +90,99 @@ function App() {
 
     if (allowedTypes.indexOf(file.type) > -1) {
       // const canvas = ReactDOM.findDOMNode(photo);
-      const canvas = photo.current;
-      let image = new Image();
+      // const canvas = photo.current;
+      // let image = new Image();
 
       // note case!
       // image.onload = () => { renderImage(canvas, image); };
-      image.src = window.URL.createObjectURL(file);
+      // image.src = window.URL.createObjectURL(file);
 
       // handleImageUpload(window.URL.createObjectURL(file));
-      if (canvas) {
-        // @ts-ignore
-        const context = canvas.getContext('2d')
-        // @ts-ignore
-        handleImageUpload(context.getImageData(0, 0, canvas.width, canvas.height), {});
-      }
-      setFile(image)
+      // if (canvas) {
+      // @ts-ignore
+      // const context = canvas.getContext('2d')
+      // @ts-ignore
+      // handleImageUpload(context.getImageData(0, 0, canvas.width, canvas.height), {});
+      // }
+      setFile(window.URL.createObjectURL(file));
     } else {
       setHasError(true);
       setErrorMessage('Image type not recognised');
     }
-  }
+  };
+  const processForm = (e: FormEvent) => {
+    e.preventDefault();
+    const canvas = photo.current;
+    console.log('processing');
+    console.log(canvas, file);
+    if (canvas && file) {
+      // @ts-ignore
+      const context = canvas.getContext('2d');
+      // showImagePreview(context, file);
+      handleImageUpload(
+        // @ts-ignore
+        context.getImageData(0, 0, canvas.width, canvas.height),
+        // @ts-ignore
+        {},
+      );
+    }
+  };
 
-      const classes = classNames({
-      'upload': true,
-      'upload--drag-enter': dragEnter,
-      // 'upload--has-file': this.state.preview,
-      // 'upload--error': this.state.error
-    });
+  const classes = classNames({
+    upload: true,
+    'upload--drag-enter': dragEnter,
+    // 'upload--has-file': this.state.preview,
+    // 'upload--error': this.state.error
+  });
 
-    const errorClasses = classNames({
-      'error-message': true,
-      // 'error-message--visible': this.state.error
-    });
+  const errorClasses = classNames({
+    'error-message': true,
+    // 'error-message--visible': this.state.error
+  });
   return (
     <div className="App">
-    {/* <UploadForm converter={noWorkerConverter}/> */}
-    <BackendForm />
-      <div
-          className={ classes }
-          onClick={ onClick }
-          onDrop={ onDrop }
-          onDragEnter={ onDragEnter }
-          onDragOver={ onDragOver }
-          onDragLeave={ onDragLeave }>
+      {/* <UploadForm converter={noWorkerConverter}/> */}
+      <BackendForm />
+      <canvas ref={photo} className="canvas"></canvas>
+      <form onSubmit={processForm}>
+        {file ? (
+          <Preview file={file} clear={setFile} />
+        ) : (
+          <div
+            onClick={onClick}
+            onDrop={onDrop}
+            className={classes}
+            onDragEnter={onDragEnter}
+            onDragOver={onDragOver}
+            onDragLeave={onDragLeave}
+          >
+            <div className="instructions instructions--standard">
+              Drag an image here, or{' '}
+              <button className="button icon-arrow-up">upload</button>
+            </div>
+            <span className="instructions instructions--drop">Drop it!</span>
 
-          <div className='instructions instructions--standard'>
-            Drag an image here, or <button className='button icon-arrow-up'>upload</button>
+            <input
+              type="file"
+              ref={input}
+              onChange={onDrop}
+              className="input"
+            />
           </div>
-          <span className='instructions instructions--drop'>
-            Drop it!
-          </span>
-
-          <input type='file' ref={input} onChange={ onDrop } className='input' />
-          <canvas ref={photo} className='canvas'></canvas>
-        </div>
+        )}
+      </form>
     </div>
   );
 }
+
+const Preview = ({ file, clear }: { file: string; clear: any }) => {
+  return (
+    <>
+      <img src={file} className="preview" />
+      <button type="submit">Convert</button>
+      <button onClick={() => clear()}>Clear</button>
+    </>
+  );
+};
 
 export default App;
