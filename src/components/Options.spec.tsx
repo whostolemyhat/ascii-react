f0eff0ab7c1d5d23a9b734c503975de852a0eea6
James Baum <james@jamesbaum.co.uk>
2022-08-11 09:11:03 +0100

    Add Options tests

--- src/components/Options.spec.tsx
+++ src/components/Options.spec.tsx
@@ -3,8 +3,94 @@ import { Converter, OutputType } from '../utils/types';
 import { Options } from './Options';
 
 describe('Options', () => {
-  it.todo('should set selected converter');
-  it.todo('should set selected output');
+  it('should set selected converter', () => {
+    let renderOptions = render(
+      <Options
+        setConverter={jest.fn()}
+        setOutput={jest.fn()}
+        converter={Converter.SharedPool}
+        output={OutputType.Text}
+      />,
+    );
+
+    expect(
+      (
+        renderOptions.container.querySelector(
+          '[name=converter]:checked',
+        ) as HTMLInputElement
+      ).value,
+    ).toEqual('sharedpool');
+
+    renderOptions = render(
+      <Options
+        setConverter={jest.fn()}
+        setOutput={jest.fn()}
+        converter={Converter.None}
+        output={OutputType.Text}
+      />,
+    );
+
+    expect(
+      (
+        renderOptions.container.querySelector(
+          '[name=converter]:checked',
+        ) as HTMLInputElement
+      ).value,
+    ).toEqual('none');
+
+    renderOptions = render(
+      <Options
+        setConverter={jest.fn()}
+        setOutput={jest.fn()}
+        converter={Converter.Buffer}
+        output={OutputType.Text}
+      />,
+    );
+
+    expect(
+      (
+        renderOptions.container.querySelector(
+          '[name=converter]:checked',
+        ) as HTMLInputElement
+      ).value,
+    ).toEqual('buffer');
+  });
+
+  it('should set selected output', () => {
+    let renderOptions = render(
+      <Options
+        setConverter={jest.fn()}
+        setOutput={jest.fn()}
+        converter={Converter.SharedPool}
+        output={OutputType.Text}
+      />,
+    );
+
+    expect(
+      (
+        renderOptions.container.querySelector(
+          '[name=output]:checked',
+        ) as HTMLInputElement
+      ).value,
+    ).toEqual('text');
+
+    renderOptions = render(
+      <Options
+        setConverter={jest.fn()}
+        setOutput={jest.fn()}
+        converter={Converter.None}
+        output={OutputType.Image}
+      />,
+    );
+
+    expect(
+      (
+        renderOptions.container.querySelector(
+          '[name=output]:checked',
+        ) as HTMLInputElement
+      ).value,
+    ).toEqual('image');
+  });
 
   describe('converters', () => {
     it('should select single-thread', () => {
@@ -23,12 +109,70 @@ describe('Options', () => {
       expect(setConverter).toHaveBeenCalledWith(Converter.None);
     });
 
-    it.todo('should select single worker');
-    it.todo('should select multi-worker');
+    it('should select single worker', () => {
+      const setConverter = jest.fn();
+      render(
+        <Options
+          setConverter={setConverter}
+          setOutput={jest.fn()}
+          converter={Converter.SharedPool}
+          output={OutputType.Text}
+        />,
+      );
+      const button = screen.getByText('Background worker');
+      fireEvent.click(button);
+
+      expect(setConverter).toHaveBeenCalledWith(Converter.Buffer);
+    });
+
+    it('should select multi-worker', () => {
+      const setConverter = jest.fn();
+      render(
+        <Options
+          setConverter={setConverter}
+          setOutput={jest.fn()}
+          converter={Converter.None}
+          output={OutputType.Text}
+        />,
+      );
+      const button = screen.getByText('Multi-worker');
+      fireEvent.click(button);
+
+      expect(setConverter).toHaveBeenCalledWith(Converter.SharedPool);
+    });
   });
 
   describe('output', () => {
-    it.todo('should select text');
-    it.todo('should select image');
+    it('should select text', () => {
+      const setOutput = jest.fn();
+      render(
+        <Options
+          setConverter={jest.fn()}
+          setOutput={setOutput}
+          converter={Converter.SharedPool}
+          output={OutputType.Image}
+        />,
+      );
+      const button = screen.getByText('Text');
+      fireEvent.click(button);
+
+      expect(setOutput).toHaveBeenCalledWith(OutputType.Text);
+    });
+
+    it('should select image', () => {
+      const setOutput = jest.fn();
+      render(
+        <Options
+          setConverter={jest.fn()}
+          setOutput={setOutput}
+          converter={Converter.SharedPool}
+          output={OutputType.Text}
+        />,
+      );
+      const button = screen.getByText('Image');
+      fireEvent.click(button);
+
+      expect(setOutput).toHaveBeenCalledWith(OutputType.Image);
+    });
   });
 });
